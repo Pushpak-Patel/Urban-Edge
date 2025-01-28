04ac305ae36ba265e11baad750717d175d668cc1
Pushpak Patel <pushpak3003@gmail.com>
2025-01-28 18:05:42 +0530

    changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,29 +1,29 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
-      '/api' : {
-        target: 'http://localhost:3000',
-        secure: false,
-      },
-    },
+      "/api": {
+        target: "http://localhost:3000",
+        secure: false
+      }
+    }
   },
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['react', 'react-dom'],
-    },
+      external: ["react", "react-dom"]
+    }
   },
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src"
     },
+    extensions: [".js", ".jsx", ".ts", ".tsx"]
   },
   esbuild: {
-    jsx: 'automatic',
-  },
-
+    jsx: "automatic"
+  }
 });
